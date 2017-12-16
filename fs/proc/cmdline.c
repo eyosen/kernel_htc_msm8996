d439d7e0d36f055a08395e64ddec2f82110cb675
tbalden <illespal@gmail.com>
2017-12-16 04:52:30 -0800

    cmdline: verified bootstate check override with magisk ignore
    adding check magisk code to verify if magisk boot image.
    If so do not overried cmdline params, it causes inability
    to boot on Oreo.
    
    If non-magisk, replace android.bootstateverified with aaaa...
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -6,8 +6,46 @@
 
 static char new_command_line[COMMAND_LINE_SIZE];
 
+#if 1
+#include <linux/slab.h>
+#include <linux/spinlock.h>
+
+static bool done = false;
+
+static DEFINE_SPINLOCK(show_lock);
+
+static const char *replace =      "androidboot.verifiedbootstate=";
+static char *replace_with =       "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa=";
+
+static bool magisk = true;
+
+extern bool is_magisk(void);
+extern bool is_magisk_sync(void);
+extern void init_magisk(void);
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#if 1
+	spin_lock(&show_lock);
+	if (done) {
+	} else {
+		magisk = is_magisk();
+		if (!magisk) {
+			char *tmp = saved_command_line;
+			tmp = strstr(tmp,replace);
+			if (tmp) {
+				while ((*replace_with)!='\0') {
+					*tmp = *replace_with;
+					replace_with++;
+					tmp++;
+				}
+			}
+		}
+		done = true;
+	}
+	spin_unlock(&show_lock);
+#endif
 	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
@@ -26,6 +64,9 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
+#if 1
+	init_magisk();
+#endif
 	char *offset_addr, *cmd = new_command_line;
 
 	strcpy(cmd, saved_command_line);
